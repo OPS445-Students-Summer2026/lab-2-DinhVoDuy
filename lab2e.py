84c1ef78166774aa2ae4b60d28263a9a1d8d8e6b
Dinh Vo <dvo12@myseneca.ca>
2026-05-22 13:35:47 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Dinh Vo
+#Author ID: 114641244
+#Data Created: 2026/05/22
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer = timer -1
+
+print("blast off!")
