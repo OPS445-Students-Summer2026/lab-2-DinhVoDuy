bfa11d2400ed0b8021a770676c6c5e6b4cf93927
Dinh Vo <dvo12@myseneca.ca>
2026-05-22 13:41:26 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Dinh Vo
+#Author ID: 114641244
+#Data Created: 2026/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer -1
+
+print("blast off!")
